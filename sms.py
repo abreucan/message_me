6d7049d4c87951094185db73fc3d6e357715e39d
Andrea Abreu <79025012+abreucan@users.noreply.github.com>
2023-05-31 11:48:14 -0500

    Update sms.py

--- sms.py
+++ sms.py
@@ -3,8 +3,8 @@ import os
 from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
-account_sid = "AC04cd39d30875af9538b76b8970649cc7"
-auth_token = '09eff29012754bc6a76c8cf8f05361d3'
+account_sid = confidential
+auth_token = confidential
 client = Client(account_sid, auth_token)
 message = client.messages.create(
   body="Hello from Twilio",
